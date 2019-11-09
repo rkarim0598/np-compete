744bf558fdd8e68d8bc69a097a6cd8c54edcd42c
Stephen Hannon <stephenhannon4@gmail.com>
2019-11-09 12:42:08 -0500

    Adds material

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/nonprofits">All nonprofits</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<md-toolbar>
+			<h1><router-link to="/">NP Complete</router-link></h1>
+			<md-button to="/nonprofits">All nonprofits</md-button>
+			<md-button to="/about">About</md-button>
+		</md-toolbar>
+		<md-content>
+			<router-view/>
+		</md-content>
+	</div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+h1 {
+	flex: 1;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+
 import { firestorePlugin } from 'vuefire'
 
+import { MdButton, MdContent, MdTabs, MdToolbar } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
 Vue.use(firestorePlugin)
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+Vue.use(MdToolbar)
 
 Vue.config.productionTip = false
 
