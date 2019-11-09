9994409017df220b64e3021df9dd4d05e36cce50
Stephen Hannon <stephenhannon4@gmail.com>
2019-11-09 00:00:32 -0500

    Adds firebase functions

---
+++ firebase/functions/index.js
@@ -0,0 +1,45 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.addVotes = functions.firestore
+	.document('donations/{donationId}')
+	.onCreate((snap, context) => {
+		const npid = snap.data().npid;
+		if (!npid) return null;
+
+		const npRef = db.doc(`nonprofits/${npid}`);
+		npRef.get().then(doc => {
+			if (doc.exists) {
+				const votes = doc.get('votes') || 0;
+				doc.ref.set({
+					votes: votes + 1,
+				}, { merge: true });
+			} else {
+				console.error(`NPID ${npid} not found`);
+			}
+
+			return null;
+		}).catch(error => {
+			console.error("Error getting document:", error);
+		});
+
+		return true;
+	});
+
+exports.test = functions.firestore
+	.document('donations/{donationId}')
+	.onCreate((snap, context) => {
+		return snap.ref.set({
+			authorized: true,
+		}, { merge: true });
+	});
