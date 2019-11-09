76d1c8284c5e14fc20109d6eb89dd1c9016d0a9e
jvisbal <57573738+jvisbal@users.noreply.github.com>
2019-11-09 18:25:27 -0500

    Started about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,75 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <md-card>
+      <md-card-header>
+    <div class="md-title">Our Mission 
+
+    </div>
+      </md-card-header>
+
+      <md-card-content>
+        Our mission is to help raise awareness for local nonprofit organizations. We provided a platform for donors to use social media to impact the community as well. Working with other nonprofits we managed to acquire a pot and that money we will donate to the nonprofit to have the most votes. That way the community has a voice and some nonprofit is getting essential funds it needs to improve.
+      </md-card-content>
+
+     
+    </md-card>
+  
+
+      
+  
+
+   <md-card>
+     <md-card-header>
+       <md-card-header-text>
+         <div class="md-title"> John Meyer</div>
+         <div class="md-subhead">Senior - Likes pickles but does not like pineapples</div>
+        
+         </md-card-header-text>
+         
+      </md-card-header>
+
+   </md-card>
+   <md-card>
+     <md-card-header>
+       <md-card-header-text>
+         <div class="md-title"> Rayyan Karim</div>
+         <div class="md-subhead">Senior - He's big on home cooking. Hopes to become chef after graduating</div>
+        
+         </md-card-header-text>
+         
+      </md-card-header>
+
+   </md-card>
+   <md-card>
+     <md-card-header>
+       <md-card-header-text>
+         <div class="md-title"> Stephen Meyer</div>
+         <div class="md-subhead">Junior - I have to think about that one</div>
+        
+         </md-card-header-text>
+         
+      </md-card-header>
+
+   </md-card>
+   <md-card>
+     <md-card-header>
+       <md-card-header-text>
+         <div class="md-title"> Jorge Visbal</div>
+         <div class="md-subhead">Freshman - Krispy Kreme connoisseur</div>
+        
+         </md-card-header-text>
+         
+      </md-card-header>
+
+   </md-card>
+ </div>
 </template>
+<style lang="scss" scoped>
+ .md-card {
+   width: 320px;
+   margin: 4px;
+   display: inline-block;
+   vertical-align: top;
+ }
+ </style>
+ 
