db9bf45bdcd0a3465a1be9360b2a429b66c7f350
Stephen Hannon <stephenhannon4@gmail.com>
2019-11-09 12:22:23 -0500

    Adds firebase binding to nonprofit details page

--- src/views/Nonprofit.vue
+++ src/views/Nonprofit.vue
@@ -1,3 +1,36 @@
 <template>
-    <h2>Nonprofit {{ $route.params.id }}</h2>
+	<div>
+		<template v-if="nonprofit">
+			<h2>{{ nonprofit && nonprofit.name }}</h2>
+
+		</template>
+		<template v-else>
+			<h2>Nonprofit not found</h2>
+			<p>A nonprofit with the id "{{ $route.params.id }}" was not found. <router-link to="/">Return home</router-link>.</p>
+		</template>
+	</div>
 </template>
+
+<script>
+import { db } from '@/db'
+
+const nonprofits = db.collection('nonprofits')
+
+export default {
+	data() {
+		return {
+			nonprofit: null,
+		}
+	},
+
+	watch: {
+		$route: {
+			// call it upon creation too
+			immediate: true,
+			handler(to, from) {
+				this.$bind('nonprofit', nonprofits.doc(to.params.id))
+			},
+		},
+	},
+}
+</script>
