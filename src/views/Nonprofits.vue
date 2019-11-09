46902c6e81f6232479b6f3ddc6c8d30fe35df42b
Stephen Hannon <stephenhannon4@gmail.com>
2019-11-09 11:48:11 -0500

    Adds firebase

--- src/views/Nonprofits.vue
+++ src/views/Nonprofits.vue
@@ -1,3 +1,26 @@
 <template>
-    <h2>All nonprofits</h2>
+    <div>
+        <h2>All nonprofits</h2>
+        <ul v-for="nonprofit in nonprofits" :key="nonprofit.id">
+            <li>
+                <router-link :to="'/nonprofits/' + nonprofit.id">{{ nonprofit.name }}</router-link>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<script>
+import { db } from '@/db'
+
+export default {
+  data() {
+    return {
+      nonprofits: [],
+    }
+  },
+
+  firestore: {
+    nonprofits: db.collection('nonprofits'),
+  },
+}
+</script>
