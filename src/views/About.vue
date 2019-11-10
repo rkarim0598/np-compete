bbe2ece0442108ecbff0927d32c1c5ec8ee321a3
Stephen Hannon <stephenhannon4@gmail.com>
2019-11-09 23:45:18 -0500

    Updates about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,14 @@
 <template>
-  <div>
-    <span class="md-layout-item md-title">Our Mission</span>
-    <br />
-    <span
-      class="md-layout-item md-subheading"
-    >Our mission is to help raise awareness for local nonprofit organizations. We provided a platform for donors to use social media to impact the community as well. Working with other nonprofits we managed to acquire a pot and that money we will donate to the nonprofit to have the most votes. That way the community has a voice and some nonprofit is getting essential funds it needs to improve.</span>
-    <h1 class="md-title">The Team</h1>
-    <div class="md-layout-item">
+  <div class="about">
+    <h2>Our Mission</h2>
+    <p>Our mission is to help raise awareness for local nonprofit organizations. We provided a platform for donors to use social media to impact the community as well. Working with other nonprofits we managed to acquire a pot and that money we will donate to the nonprofit to have the most votes. That way the community has a voice and some nonprofit is getting essential funds it needs to improve.</p>
+
+    <h2>The Team</h2>
       <md-card>
         <md-card-header>
           <md-card-header-text>
             <div class="md-title">John Meyer</div>
-            <div class="md-subhead">Senior - Dunne Hall , part of Notre Dame Chorale</div>
+            <div class="md-subhead">Senior &mdash; Dunne Hall, part of Notre Dame Chorale</div>
           </md-card-header-text>
         </md-card-header>
       </md-card>
@@ -20,17 +17,16 @@
         <md-card-header>
           <md-card-header-text>
             <div class="md-title">Rayyan Karim</div>
-            <div class="md-subhead">Senior - Former Otter</div>
+            <div class="md-subhead">Senior &mdash; Former Otter</div>
           </md-card-header-text>
         </md-card-header>
       </md-card>
-    </div>
-    <div class="md-layout-item last-column">
+      
       <md-card>
         <md-card-header>
           <md-card-header-text>
-            <div class="md-title">Stephen Meyer</div>
-            <div class="md-subhead">Junior - Sorin Hall, part of The Observer</div>
+            <div class="md-title">Stephen Hannon</div>
+            <div class="md-subhead">Junior &mdash; Sorin Hall, part of The Observer</div>
           </md-card-header-text>
         </md-card-header>
       </md-card>
@@ -38,22 +34,18 @@
         <md-card-header>
           <md-card-header-text>
             <div class="md-title">Jorge Visbal</div>
-            <div class="md-subhead">Freshman - Alumni Hall, Spikeball club</div>
+            <div class="md-subhead">Freshman &mdash; Alumni Hall, Spikeball club</div>
           </md-card-header-text>
         </md-card-header>
       </md-card>
-    </div>
   </div>
 </template>
 <style lang="scss" scoped>
+.about {
+  padding: 30px;
+}
 .full-width {
   width: 100%;
-}
-.last-column {
-  margin-left: 0;
-}
-.md-layout-item {
-  height: 100%;
 }
 </style>
  
